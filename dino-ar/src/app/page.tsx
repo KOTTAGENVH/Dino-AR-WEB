25b267338df15ad2d88b7ae65209cf4c4b887db2
Kottage N.V.H <nowendevelopment@gmail.com>
2024-05-18 04:16:38 +0530

    feat: added home page

--- dino-ar/src/app/page.tsx
+++ dino-ar/src/app/page.tsx
@@ -4,6 +4,7 @@ import Header from "@/components/header";
 import TiltImage from "@/components/tilt_image";
 import { motion } from "framer-motion";
 import { Inter } from "@next/font/google";
+import { useMediaQuery } from "react-responsive";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -11,24 +12,29 @@ const inter = Inter({
 });
 
 export default function Home() {
+  const isMobile = useMediaQuery({ query: "(max-width: 767px)" });
+  const xValue = isMobile ? 20 : 100;
+
   return (
     <div className="flex flex-col h-screen justify-between">
       <Header />
       <main
-        className={`mb-auto h-10 bg-black-500 h-full p-7 ${inter.className}`}
+        className={`mb-auto h-10 bg-black-500 h-full ${inter.className} p-0 xl:p-7`}
       >
-        <div className="flex flex-row m-4  h-max flex-auto flex-wrap">
-          <div className="mr-2 p-1.5 mt-24 h-auto">
+        <div className="flex flex-row xl:m-4  h-max flex-auto flex-wrap">
+          <div className="xl:mr-2 p-1.5 xl:mt-24 mt-0 h-auto">
             <TiltImage url="/wallpaper.jpeg" />
           </div>
-          <div className="flex flex-col w-1/3">
+          <div className="flex flex-col xl:w-1/3 w-56 p-1.5 w-11/12">
             <motion.div
-              animate={{ x: 100 }}
+              animate={{ x: xValue }}
               transition={{ ease: "linear", duration: 2, x: { duration: 1 } }}
             >
-              <div className="p-1.5 text-6xl">Dino AR</div>
-              <div className="p-1.5 text-xl mt-24">
-                <p>
+              <div className="xl:p-1.5 text-2xl xl:text-6xl text-left">
+                Dino AR
+              </div>
+              <div className="p-1.5 xl:text-xl xl:mt-24 text-base text-left  w-full">
+                <p className=" px-8 w-11/12 xl:px-0">
                   Dino AR is an augmented reality programme that lets users
                   visualise dinosaurs in their surroundings using a marker.
                   Users can access details about several dinosaurs on the web
@@ -37,7 +43,7 @@ export default function Home() {
                   realistic experience.
                 </p>
               </div>
-              <button className="p-1.5 bg-black-500 text-white mt-24 hover:bg-sky-700 rounded-xl">
+              <button className="p-1.5 bg-sky-700 lg:bg-black-500 text-white mt-16 xl:mt-24 hover:bg-teal-700 rounded-xl">
                 <a href="/encyclopedia">
                   Time Travel <span className="ml-1">&#8594;</span>
                 </a>
