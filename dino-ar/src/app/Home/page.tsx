e65cb2d25ed1e5f5bf024ac2d66369dbf14b707d
Kottage N.V.H <nowendevelopment@gmail.com>
2024-05-17 15:37:17 +0530

    feat: custom full page carousel

---
+++ dino-ar/src/app/Home/page.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+
+function Home () {
+  return (
+    <div
+    style={{
+        height: '102vh',
+        width: '99.1vw',
+        backgroundColor: 'green',
+    }}
+    >
+        <div
+        style={{
+        margin: '10px',
+        }}
+        >
+        Home
+        </div>
+        
+    </div>
+  )
+}
+
+
+export default Home
---
+++ dino-ar/src/app/legal/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Legal() {
+  return (
+    <div
+    style={{
+        height: '100vh',
+        width: '99.1vw',
+        backgroundColor: 'red',
+    }}
+    >
+        page
+        </div>
+  )
+}
+
+export default Legal
