d67fc4ade309d810313e1556f20a2d87921bb637
Kottage N.V.H <nowendevelopment@gmail.com>
2024-05-18 16:00:36 +0530

    Update page.tsx

--- dino-ar/src/app/Encyclopedia/page.tsx
+++ dino-ar/src/app/Encyclopedia/page.tsx
@@ -8,32 +8,32 @@ function Encyclopedia(){
   const legalRef = useRef<HTMLDivElement>(null);
   const lastScrollTime = useRef<number>(0);
 
-  useEffect(() => {
-    const handleScroll = (event: WheelEvent) => {
-      const currentTime = new Date().getTime();
-      const timeDiff = currentTime - lastScrollTime.current;
+  // useEffect(() => {
+  //   const handleScroll = (event: WheelEvent) => {
+  //     const currentTime = new Date().getTime();
+  //     const timeDiff = currentTime - lastScrollTime.current;
 
-      if (homeRef.current && legalRef.current) {
-        const homeHeight = homeRef.current.clientHeight;
-        const deltaY = event.deltaY;
+  //     if (homeRef.current && legalRef.current) {
+  //       const homeHeight = homeRef.current.clientHeight;
+  //       const deltaY = event.deltaY;
 
-        if (deltaY > 0 && timeDiff > 1000) {
-          // Scrolling down
-          legalRef.current.scrollIntoView({ behavior: "smooth" });
-          lastScrollTime.current = currentTime;
-        } else if (deltaY < 0) {
-          // Scrolling up
-          homeRef.current.scrollIntoView({ behavior: "smooth" });
-          lastScrollTime.current = currentTime;
-        }
-      }
-    };
+  //       if (deltaY > 0 && timeDiff > 1000) {
+  //         // Scrolling down
+  //         legalRef.current.scrollIntoView({ behavior: "smooth" });
+  //         lastScrollTime.current = currentTime;
+  //       } else if (deltaY < 0) {
+  //         // Scrolling up
+  //         homeRef.current.scrollIntoView({ behavior: "smooth" });
+  //         lastScrollTime.current = currentTime;
+  //       }
+  //     }
+  //   };
 
-    window.addEventListener("wheel", handleScroll);
-    return () => {
-      window.removeEventListener("wheel", handleScroll);
-    };
-  }, []);
+  //   window.addEventListener("wheel", handleScroll);
+  //   return () => {
+  //     window.removeEventListener("wheel", handleScroll);
+  //   };
+  // }, []);
 
   return (
     <div>
