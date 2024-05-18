11b2ebf2d36dd866e106adb3b4f819616effd1da
Kottage N.V.H <nowendevelopment@gmail.com>
2024-05-18 23:54:52 +0530

    feat: added a marker

--- dino-ar/src/components/dinosaurs/animated_t_rex.tsx
+++ dino-ar/src/components/dinosaurs/animated_t_rex.tsx
@@ -2,8 +2,17 @@ import React from 'react'
 
 function AnimatedTrex() {
   return (
-    <div className="bg-animated_t_rex bg-center bg-cover  h-full w-full">
-      </div>
+<div className="bg-animated_t_rex bg-center bg-cover h-full w-full p-1">
+<div className="flex flex-row flex-wrap  bg-opacity-50 backdrop-blur-md h-4/5 w-10/12 xl:w-11/12 m-4 mt-8 xl:m-12 p-8 rounded-2xl shadow-lg justify-between">
+    <div className="bg-white xl:w-5/12 xl:h-5/6 w-80 m-4 rounded-2xl">
+
+    </div>
+    <div className="bg-marker bg-contain xl:w-5/12 xl:h-5/6 w-80 m-4 rounded-2xl">
+
+    </div>
+  </div>
+</div>
+
   )
 }
 
