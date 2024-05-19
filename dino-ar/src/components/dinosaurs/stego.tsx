31f6afc446135491f41e535d0360fa396b546b81
Kottage N.V.H <nowendevelopment@gmail.com>
2024-05-19 18:38:18 +0530

    fix: lack of visibility @stego

--- dino-ar/src/components/dinosaurs/stego.tsx
+++ dino-ar/src/components/dinosaurs/stego.tsx
@@ -10,7 +10,7 @@ function Stego() {
   return (
     <div className="bg-stego bg-center bg-cover h-full w-full p-1">
       <div className="flex flex-row flex-wrap  bg-opacity-50 backdrop-blur-md h-4/5 w-10/12 xl:w-11/12 m-4 mt-8 xl:m-12 p-8 rounded-2xl shadow-lg justify-between">
-        <div className="text-white xl:w-5/12 xl:h-5/6 w-80 m-4 rounded-2xl p-5">
+        <div className="text-black xl:w-5/12 xl:h-5/6 w-80 m-4 rounded-2xl p-5">
           <p className={`${inter.className} underline text-2xl m-2`}>
             Stegosaurus
           </p>
