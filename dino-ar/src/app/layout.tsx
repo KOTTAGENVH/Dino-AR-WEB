f405304871a5f47df02c8404f2fed48695c2a582
Kottage N.V.H <nowendevelopment@gmail.com>
2024-05-20 19:10:48 +0530

    Update layout.tsx

--- dino-ar/src/app/layout.tsx
+++ dino-ar/src/app/layout.tsx
@@ -8,7 +8,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Dino-AR",
   description: "Established in 2024",
-  metadataBase: new URL("https://dino-ar-teal.vercel.app"),
+  metadataBase: new URL("https://dino-ar-web.vercel.app"),
 };
 
 export default function RootLayout({
@@ -18,6 +18,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="Established in 2024" />
+        <meta name="robots" content="index, follow" />
+        <link rel="canonical" href="https://dino-ar-web.vercel.app" />
+        <title>DinoAR</title>
+        <link rel="icon" href="/image/favicon.ico" sizes="any" />
+      </Head>
       <body className={inter.className}>
         {/*Ban inspect elements*/}
         <script
@@ -30,7 +39,6 @@ export default function RootLayout({
             `,
           }}
         />
-        <link rel="icon" href="/image/favicon.ico" sizes="any" />
         {children}
       </body>
     </html>
